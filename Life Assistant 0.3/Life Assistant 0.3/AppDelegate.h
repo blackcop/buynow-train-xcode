c1faf590f28ea324d25289185080acd8bdedf509
wn <wn@bsfit.com.cn>
2013-05-14 12:59:29 +0800

    None.

---
+++ Life Assistant 0.3/Life Assistant 0.3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Life Assistant 0.3
+//
+//  Created by Holmes Wu on 13-5-14.
+//  Copyright (c) 2013年 BSFit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Life Assistant 0.3/Life Assistant 0.3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Life Assistant 0.3
+//
+//  Created by Holmes Wu on 13-5-14.
+//  Copyright (c) 2013年 BSFit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
