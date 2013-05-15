5dba56949b6714ae939b3d8be6971521df515263
huang <hff@bsfit.com.cn>
2013-05-15 10:11:38 +0800

    add a class for 地域筛选view controller by huang

---
+++ Life Assistant 0.4/Life Assistant 0.4/regionalScreeningViewController.m
@@ -0,0 +1,38 @@
+//
+//  regionalScreeningViewController.m
+//  Life Assistant 0.4
+//
+//  Created by 邦盛 on 13-5-15.
+//  Copyright (c) 2013年 BSFit. All rights reserved.
+//
+
+#import "regionalScreeningViewController.h"
+
+@interface regionalScreeningViewController ()
+
+@end
+
+@implementation regionalScreeningViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
