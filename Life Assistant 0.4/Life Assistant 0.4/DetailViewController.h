4fade69906e111d3c2e596bc3144fd09dac8e96e
wn <wn@bsfit.com.cn>
2013-05-16 16:59:12 +0800

    add map classes by wu.

---
+++ Life Assistant 0.4/Life Assistant 0.4/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Life Assistant 0.4
+//
+//  Created by Holmes Wu on 13-5-16.
+//  Copyright (c) 2013年 BSFit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ Life Assistant 0.4/Life Assistant 0.4/MapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewController.m
+//  Life Assistant 0.4
+//
+//  Created by Holmes Wu on 13-5-16.
+//  Copyright (c) 2013年 BSFit. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
