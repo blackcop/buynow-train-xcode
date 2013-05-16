2eb0d52cff322b23abfacb818a042d520f7b2350
john <johntai@foxmail.com>
2013-05-16 14:43:14 +0800

    modify Association page

---
+++ Life Assistant 0.4/Life Assistant 0.4/AssociationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AssociationViewController.h
+//  Life Assistant 0.4
+//
+//  Created by john on 13-5-16.
+//  Copyright (c) 2013年 BSFit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssociationViewController : UITableViewController
+
+@end
