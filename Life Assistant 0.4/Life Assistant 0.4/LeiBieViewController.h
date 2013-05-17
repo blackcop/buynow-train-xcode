ea0e5199437b287009003b1b83e9d85b1bcf2d32
huang <hff@bsfit.com.cn>
2013-05-17 15:35:38 +0800

    leibie by huang

---
+++ Life Assistant 0.4/Life Assistant 0.4/LeiBieViewController.h
@@ -0,0 +1,16 @@
+//
+//  LeiBieViewController.h
+//  Life Assistant 0.4
+//
+//  Created by 邦盛 on 13-5-17.
+//  Copyright (c) 2013年 BSFit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeiBieViewController : UIViewController
+- (IBAction)b1:(id)sender;
+- (IBAction)b2:(id)sender;
+- (IBAction)b3:(id)sender;
+
+@end
---
+++ Life Assistant 0.4/Life Assistant 0.4/LeiBieViewController.m
@@ -0,0 +1,45 @@
+//
+//  LeiBieViewController.m
+//  Life Assistant 0.4
+//
+//  Created by 邦盛 on 13-5-17.
+//  Copyright (c) 2013年 BSFit. All rights reserved.
+//
+
+#import "LeiBieViewController.h"
+
+@interface LeiBieViewController ()
+
+@end
+
+@implementation LeiBieViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)b1:(id)sender {
+}
+
+- (IBAction)b2:(id)sender {
+}
+- (IBAction)b3:(id)sender {
+}
+@end
